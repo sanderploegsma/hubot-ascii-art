a7f1241c47fa00e00a9e7e6706dad1a6b75f2297
Tim van der Lippe <tvanderlippe@gmail.com>
2017-12-11 11:34:16 +0100

    Fix loading logic and fix tests

--- index.js
+++ index.js
@@ -12,18 +12,8 @@ const path = require('path');
 module.exports = function(robot, scripts) {
   const scriptsPath = path.resolve(__dirname, 'src');
   if (fs.existsSync(scriptsPath)) {
-    return (() => {
-      const result = [];
-      for (let script of Array.from(fs.readdirSync(scriptsPath).sort())) {
-        if ((scripts != null) && !Array.from(scripts).includes('*')) {
-          if (Array.from(scripts).includes(script)) { result.push(robot.loadFile(scriptsPath, script)); } else {
-            result.push(undefined);
-          }
-        } else {
-          result.push(robot.loadFile(scriptsPath, script));
-        }
-      }
-      return result;
-    })();
+    return fs.readdirSync(scriptsPath)
+      .filter(script => scripts == null || scripts.includes('*') || scripts.includes(script))
+      .map(script => robot.loadFile(scriptsPath, script));
   }
 };
