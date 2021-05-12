36f3b4f506105d1f06fcf096328d238df29997a3
Sander Ploegsma <sander.ploegsma@asml.com>
2021-05-12 16:37:08 +0200

    Clean up code

--- src/ascii.js
+++ src/ascii.js
@@ -9,27 +9,42 @@
 
 const figlet = require("figlet");
 
+const knownAdapters = {
+  slack: "slack",
+  hipchat: "hipchat",
+};
+
+function format(ascii, adapter) {
+  // To prevent adapters trimming whitespace at the beginning,
+  // wrap the ASCII in a code environment.
+  switch (adapter) {
+    case knownAdapters.slack:
+      return `\`\`\`${ascii}\`\`\``;
+    case knownAdapters.hipchat:
+      return `/code ${ascii}`;
+    default:
+      return `\n${ascii}`;
+  }
+}
+
+function transform(text, adapter, cb) {
+  figlet.text(text, "Standard", (err, data) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, format(data, adapter));
+    }
+  });
+}
+
 module.exports = (robot) => {
   robot.respond(/ascii (.+)/i, (res) => {
-    if (res.match[1]) {
-      return figlet.text(res.match[1], "Standard", (err, data) => {
-        if (!err) {
-          // To prevent adapters trimming whitespace at the beginning,
-          // wrap the ASCII in a code environment.
-          switch (robot.adapterName) {
-            case "slack":
-              return res.send(`\`\`\`${data}\`\`\``);
-            case "hipchat":
-              return res.send(`/code ${data}`);
-            default:
-              return res.send(`\n${data}`);
-          }
-        } else {
-          return res.send(`Oops, something went wrong! Error: ${err}`);
-        }
-      });
-    } else {
-      return res.send("Oh no! You must supply text to convert!");
-    }
+    transform(res.match[1], robot.adapterName, (err, data) => {
+      if (err) {
+        return res.send(`Oops, something went wrong! Error: ${err}`);
+      }
+
+      return res.send(data);
+    });
   });
 };
